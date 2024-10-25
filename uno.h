01a100c23ce0df1eaff2cf01eabc2298f45a45dd
Abbi :0 <166131718+Tiredabee@users.noreply.github.com>
2024-10-25 18:39:26 -0400

    Update uno.h

--- uno.h
+++ uno.h
@@ -1,15 +1,46 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <algorithim>
+#include <cstdlib>
+#include <ctime>
 
+enum class COLOR { RED, GREEN, BLUE, YELLOW, NONE };
+
+enum class TYPE { NUMBER, SKIP, REVERSE, DRAW_TWO, WILD, WILD_DRAW_FOUR };
 
 class Card {
+  public:
+      Card(Color color, Type type, int value = -1);
+      std::string toString() const;
+      Color getColor() const;
+      Type getType() const;
 
+  private:
+      Color color;
+      Type type;
+      int value;
 }
 
 class Deck {
+  public:
+      Deck();
+      void shuffle();
+      Card draw();
+      bool isEmpty() const;
 
+  private:
+      std::vector<Card> cards;
+      void initializeDeck();
 }
 
 class Player {
+  public:
+    Player(const std:: string &name);
+    void drawCard(Deck &deck);
+    void showHand() const;
+    std::string getName() const;
 
+  private:
+      std::vector<Card> hand;
 }
