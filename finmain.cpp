f5d3b90ca2953618e63e691f16b3c34a3239310f
Abbi :0 <166131718+Tiredabee@users.noreply.github.com>
2024-12-03 17:05:57 -0500

    Create finmain.cpp

---
+++ finmain.cpp
@@ -0,0 +1,29 @@
+                                                                                                                                                                                                      
+#include "uno.h"
+#include <iostream>
+
+
+int main() {
+    int numPlayers;
+    std::cout << "Enter the number of players (2-6): ";
+    std::cin >> numPlayers;
+
+    if (numPlayers < 2 || numPlayers > 6) {
+        std::cerr << "Invalid number of players. Must be between 2 and 6.\n";
+        return 1;
+    }
+
+    UnoGame game(numPlayers);
+
+    while (true) {
+        game.showGameState();
+        game.takeTurn();
+
+        if (game.checkWin()) {
+            std::cout << "Game over!" << std::endl;
+            break;
+        }
+    }
+
+    return 0;
+}
