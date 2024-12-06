3fd0bf4cbbbd7a94cf71c684e7ebc46d4a305fe4
nwhidden05 <nwhidden05@gmail.com>
2024-12-05 22:24:46 -0500

    Update finuno.cpp

--- finuno.cpp
+++ finuno.cpp
@@ -4,7 +4,10 @@
  *
  * For our final lab we decided to create an Uno game. It allows for 2-6 players to play, deals them a hand of 7 random
  * cards, and keeps track of a discard pile. It correctly checks for the colors of discarded cards as well as face value,
- * and enforces the official rules of Uno throughout the game. */ 
+ * and enforces the official rules of Uno throughout the game. 
+ *
+ * finuno.cpp runs almost the entirety of the game, including initializing the cards, deck, and players as well as the
+ * gameplay. */ 
 
 #include "uno.h"
 #include <algorithm>
@@ -14,13 +17,15 @@
 #include <ctime>
 #include <string>
 
-
-// Card Class
+// Card Class - this class contains functions that concern specific cards
 Card::Card(Color color, Type type, int value)
     : color(color), type(type), value(value) {}
 
+    // Assigns colors to the Uno cards as a string value
     std::string Card::colortoString(Color color) const {
         std::string colorStr;
+
+        // Switch statements made it easier to assign the correct color to the cards over doing if/else statements
         switch (color) {
             case Color::RED: colorStr = "Red"; break;
             case Color::GREEN: colorStr = "Green"; break;
@@ -33,6 +38,7 @@ Card::Card(Color color, Type type, int value)
         return colorStr;
     }
 
+// Assigns the type (number/action cards) to each card
 std::string Card::typetoString(Type type) const {
     std::string typeStr;
     switch (type) {
@@ -44,23 +50,26 @@ std::string Card::typetoString(Type type) const {
         case Type::WILD_DRAW_FOUR: typeStr = "Wild Draw Four"; break;
         default: return "Invalid Type";
     }
-
+    
     return typeStr;
 }
 
+// Takes the color and type of each card and turns that description into a string
 std::string Card::toString() const {
     return colortoString(color) + " " + typetoString(type);
 }
 
-
+// Returns the color of the card
 Color Card::getColor() const {
     return color;
 }
 
+// Returns the type of the card
 Type Card::getType() const {
     return type;
 }
 
+// Returns the value if it is a number card
 int Card::getValue() const {
     if (type == Type::NUMBER) {
         return value;
@@ -68,23 +77,24 @@ int Card::getValue() const {
     return -1;
 }
 
+// Compares two cards to see if they are identical
 bool Card::operator==(const Card &other) const {
     return color == other.color && type == other.type && value == other.value;
 }
 
-
-// Deck Class
+// Deck Class - this class contains functions that concern the deck as a whole
 Deck::Deck() {
     initializeDeck();
 }
 
+// Uses the shuffle function to shuffle the deck
 void Deck::shuffle() {
     std::random_device rd;
     std::mt19937 g(rd());
     std::shuffle(cards.begin(), cards.end(), g);
 }
 
-
+// Draws the top card off of the deck
 Card Deck::draw() {
     if (isEmpty()) throw std::out_of_range("Deck is empty");
     Card drawnCard = cards.back();
@@ -92,17 +102,20 @@ Card Deck::draw() {
     return drawnCard;
 }
 
+// Checks to see if the deck is empty
 bool Deck::isEmpty() const {
     return cards.empty();
 }
 
+// Initializes all of the cards needed for an Uno deck
 void Deck::initializeDeck() {
-    cards.clear(); // clear the deck
+    // Clears the deck
+    cards.clear();
 
     for (int i = 0; i < 4; i++) { 
         Color color = static_cast<Color>(i);
 
-        // 1-9 (two per color)
+        // Numbers 1-9 (two per color)
         for (int number = 1; number <= 9; number++) {
             cards.push_back(Card(color, Type::NUMBER, number));
             cards.push_back(Card(color, Type::NUMBER, number));
@@ -123,12 +136,15 @@ void Deck::initializeDeck() {
     }
 }
 
+// Player Class - this class contains functions concerning the players
 Player::Player(const std::string &name) : playername(name) {}
 
+// How the players draw cards from the deck
 void Player::drawCard(Deck &deck) {
     hand.push_back(deck.draw());
 }
 
+// Shows the current player their hand
 void Player::showHand() const {
     std::cout << "\n" << playername << "'s Hand:\n";
     for (size_t i = 0; i < hand.size(); ++i) {
@@ -136,6 +152,7 @@ void Player::showHand() const {
     }
 }
 
+// Allows the current player to play a card
 bool Player::playCard(const Card &card, std::vector<Card> &discardPile) {
     auto it = std::find(hand.begin(), hand.end(), card);
     if (it != hand.end()) {
@@ -146,21 +163,24 @@ bool Player::playCard(const Card &card, std::vector<Card> &discardPile) {
     return false;
 }
 
+// Returns the current player's name
 std::string Player::getName() const {
     return playername;
 }
 
+// Checks if a player has won
 bool Player::hasWon() const {
     return hand.empty();
 }
 
+// Returns the player's hand
 const std::vector<Card>& Player::getHand() const {
     return hand;
 }
 
-
-// takes in players and starts game
+// UnoGame Class - takes in players and starts game
 UnoGame::UnoGame(int numPlayers) : currentPlayerIndex(0), reverseOrder(false), currentColor(Color::NONE), currentType(Type::NUMBER) {
+    // Taking in the players' names, and starting the game
     for (int i = 0; i < numPlayers; ++i) {
         std::string playerName;
         std::cout << "Enter name for player " << (i + 1) << ": ";
@@ -171,15 +191,19 @@ UnoGame::UnoGame(int numPlayers) : currentPlayerIndex(0), reverseOrder(false), c
     startGame();
 }
 
-// deals cards to players and draws the discard card
+// Deals cards to players and draws the discard card
 void UnoGame::startGame() {
+    // Deals each player a hand of 7 cards
     for (auto& player : players) {
         for (int i = 0; i < 7; ++i) {
             player.drawCard(deck);
         }
     }
 
+    // Draws the first card to start the game
     Card firstCard = deck.draw();
+    
+    // If the first card is a wild card, reshuffle the deck and draw a new card to start
     while (firstCard.getType() == Type::WILD || firstCard.getType() == Type::WILD_DRAW_FOUR) {
         deck.shuffle();
         deck.draw();
@@ -191,20 +215,25 @@ void UnoGame::startGame() {
     currentValue = firstCard.getValue();
 }
 
+// Shuffles the deck
 void UnoGame::shuffleDeck() {
     deck.shuffle();
 }
 
+// Controls the user interface
+// Displays the current user's name, hand, and the card on the top of the discard pile
 void UnoGame::showGameState() const {
     std::cout << "\nCurrent Player: " << players[currentPlayerIndex].getName() << std::endl;
     std::cout << "Discard Pile: " << discardPile.back().toString() << std::endl;
     players[currentPlayerIndex].showHand();
 }
 
+// Checks if the current player has won
 bool UnoGame::checkWin() const {
     return players[currentPlayerIndex].hasWon();
 }
 
+// Assigns the color value of the current card as a string
 std::string UnoGame::colorToString(Color color) const {
     switch (color) {
         case Color::RED: return "Red";
@@ -215,11 +244,11 @@ std::string UnoGame::colorToString(Color color) const {
     }
 }
 
-
-
+// Controls each player's turn
 void UnoGame::takeTurn() {
     bool gameOver = false;  // Declare the game over flag locally
 
+    // Ends if the game has been declared over
     if (gameOver) {
         std::cout << "The game is already over!" << std::endl;
         return;
@@ -358,11 +387,7 @@ void UnoGame::takeTurn() {
     nextPlayer();
 }
 
-
-
-
-
-
+// Moves onto the next player for their turn, depending on if anyone has played a reverse card or not
 void UnoGame::nextPlayer() {
     if (reverseOrder) {
         currentPlayerIndex = (currentPlayerIndex - 1 + players.size()) % players.size();
@@ -371,16 +396,19 @@ void UnoGame::nextPlayer() {
     }
 }
 
+// If someone plays a skip card
 void UnoGame::skipTurn() {
     std::cout << "Next player's turn is skipped!\n";
     nextPlayer();
 }
 
+// If someone plays a reverse card
 void UnoGame::reverseTurnOrder() {
     std::cout << "The turn order is reversed!\n";
     reverseOrder = !reverseOrder;
 }
 
+// If someone plays a draw 2 card
 void UnoGame::drawTwo() {
     std::cout << "Next player draws two cards!\n";
     nextPlayer();
@@ -388,6 +416,7 @@ void UnoGame::drawTwo() {
     players[currentPlayerIndex].drawCard(deck);
 }
 
+// If someone plays a wild draw 4 card
 void UnoGame::wildDrawFour() {
     std::cout << "Next player draws four cards!\n";
     nextPlayer();
@@ -396,7 +425,7 @@ void UnoGame::wildDrawFour() {
     }
 }
 
-// checks if the player can put down a card
+// Checks if the player can put down a card
 bool UnoGame::isValidPlay(const Card &card) const {
     if (card.getColor() == currentColor || card.getColor() == Color::NONE) {
         return true;
@@ -416,7 +445,6 @@ bool UnoGame::isValidPlay(const Card &card) const {
     }
 
     return false;
-
 }
 
 
